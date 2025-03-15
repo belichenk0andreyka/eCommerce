779e7686a08d8bcc07043a32fce5d69036b32ce8
Andrey <test@Andrey-MacBook-Pro.local>
2025-03-15 19:40:17 +0100

    add users page, filters on admin pages and add updating users

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Menu from "@/components/shared/header/menu";
 import { APP_NAME } from "@/lib/constants";
 import MainNav from "./main-nav";
-import { Input } from "@/components/ui/input";
+import AdminSearch from "@/components/admin/admin-search";
 
 export default function AdminLayout({
   children,
@@ -24,13 +24,7 @@ export default function AdminLayout({
           </Link>
           <MainNav className="mx-6" />
           <div className="ml-auto items-center flex space-x-4">
-            <div>
-              <Input
-                type="search"
-                placeholder="Search..."
-                className="md:w-[100px] lg:w-[300px]"
-              />
-            </div>
+            <AdminSearch />
             <Menu />
           </div>
         </div>
