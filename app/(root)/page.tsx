c6e611d5d671edc2f5be16260fbe31491545ea55
Andrey <test@Andrey-MacBook-Pro.local>
2025-03-16 23:39:15 +0100

    add sorting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,24 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ViewAllProductsButton from "@/components/view-all-products-button";
+
 const Homapage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
-  return <ProductList title="Newest Arrivals" data={latestProducts} />;
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList title="Newest Arrivals" data={latestProducts} />
+      <ViewAllProductsButton />
+    </>
+  );
 };
 
 export default Homapage;
